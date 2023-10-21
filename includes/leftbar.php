46d6f3d14a778d6f5d92b196d17655c9e3484a16
Niyanta-Panchal <146424762+Niyanta-Panchal@users.noreply.github.com>
2023-10-21 01:06:44 -0700

    Add files via upload

---
+++ includes/leftbar.php
@@ -0,0 +1,33 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+			<li class="ts-label"style="font-family: Arial, Helvetica, sans-serif; font-size: 25px;"><center><b>Menu</center></b></li>
+
+		
+		    <li class="nav-item has-submenu">
+		     <a class="nav-link" href="Bonafide.php" style="font-family: Arial, Helvetica, sans-serif; font-size: 16px;"><b><i class="fa fa-users"></i>Certificate Request <sup style="color:red">*</sup> </b>  </a>
+		        <ul class="submenu collapse">
+			    <li><a class="nav-link" href="Bonafide.php">Bonafied Certificate </a></li>
+		   	     <li><a class="nav-link" href="LOR.php">LOR Certificate </a></li>
+		        </ul>
+	        </li>
+			
+			<li><a href="showrequest3.php" style="font-family: Arial, Helvetica, sans-serif; font-size: 17px;"><b><i class="fa fa-users"></i>Show Status <sup style="color:red">*</sup></b></a>
+			<ul class="submenu collapse">
+			    <li><a class="nav-link" href="showrequest3.php">&nbsp;Showrequest Bonafied Certificate </a></li>
+		   	     <li><a class="nav-link" href="showrequest4.php">&nbsp;Showrequest LOR Certificate </a></li>
+		        </ul>
+			</li>
+			<!--li><a href="notification.php"><i class="fa fa-bell"></i> &nbsp;Notification<sup style="color:red">*</sup></a>
+			</li>
+			<li><a href="messages.php"><i class="fa fa-envelope"></i> &nbsp;Messages</a>
+			</li>
+			<li><a href="feedback.php"><i class="fa fa-envelope"></i> &nbsp;Feedback</a>
+			</li>
+			<li><a href="profile.php"><i class="fa fa-user"></i> &nbsp;Update Profile</a>
+			</li-->
+			</ul>
+			<p class="text-center" style="color:#12877c; margin-top: 100px; font-family: Arial, Helvetica, sans-serif; font-size: 20px">© SSAIET-NAVSARI</p>
+		</nav>
+
+		
