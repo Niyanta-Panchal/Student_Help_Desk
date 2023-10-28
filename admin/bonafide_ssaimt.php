ef02c1d4ac9b3b18f42cfea130d2becf7e8cd067
Niyanta-Panchal <146424762+Niyanta-Panchal@users.noreply.github.com>
2023-10-28 03:20:21 -0700

    Add files via upload

---
+++ admin/bonafide_ssaimt.php
@@ -0,0 +1,220 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['alogin'])==0)
+	{	
+header('location:index.php');
+}
+else{
+if(isset($_GET['del']) && isset($_GET['name']))
+{
+$id=$_GET['del'];
+$name=$_GET['name'];
+
+$sql = "delete from bonafide WHERE id=:id";
+$query = $dbh->prepare($sql);
+$query -> bindParam(':id',$id, PDO::PARAM_STR);
+$query -> execute();
+
+$sql2 = "insert into deleteduser (email) values (:fname)";
+$query2 = $dbh->prepare($sql2);
+$query2 -> bindParam(':fname',$fname, PDO::PARAM_STR);
+$query2 -> execute();
+
+$msg="Data Deleted successfully";
+}
+
+if(isset($_REQUEST['unconfirm']))
+	{
+	$aeid=intval($_GET['unconfirm']);
+	$memstatus=1;
+	$sql = "UPDATE bonafide SET status=:status WHERE  id=:aeid";
+	$query = $dbh->prepare($sql);
+	$query -> bindParam(':status',$memstatus, PDO::PARAM_STR);
+	$query-> bindParam(':aeid',$aeid, PDO::PARAM_STR);
+	$query -> execute();
+	$msg="Changes Sucessfully";
+	}
+
+	if(isset($_REQUEST['confirm']))
+	{
+	$aeid=intval($_GET['confirm']);
+	$memstatus=0;
+	$sql = "UPDATE bonafide SET status=:status WHERE  id=:aeid";
+	$query = $dbh->prepare($sql);
+	$query -> bindParam(':status',$memstatus, PDO::PARAM_STR);
+	$query-> bindParam(':aeid',$aeid, PDO::PARAM_STR);
+	$query -> execute();
+	$msg="Changes Sucessfully";
+	}
+
+
+
+
+
+ ?>
+
+<!doctype html>
+<html lang="en" class="no-js">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="theme-color" content="#3e454c">
+	
+	<title>Manage Users</title>
+
+	<!-- Font awesome -->
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<!-- Sandstone Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- Bootstrap Datatables -->
+	<link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
+	<!-- Bootstrap social button library -->
+	<link rel="stylesheet" href="css/bootstrap-social.css">
+	<!-- Bootstrap select -->
+	<link rel="stylesheet" href="css/bootstrap-select.css">
+	<!-- Bootstrap file input -->
+	<link rel="stylesheet" href="css/fileinput.min.css">
+	<!-- Awesome Bootstrap checkbox -->
+	<link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
+	<!-- Admin Stye -->
+	<link rel="stylesheet" href="css/style.css">
+  <style>
+
+	.errorWrap {
+    padding: 10px;
+    margin: 0 0 20px 0;
+	background: #dd3d36;
+	color:#fff;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+.succWrap{
+    padding: 10px;
+    margin: 0 0 20px 0;
+	background: #5cb85c;
+	color:#fff;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+
+		</style>
+
+</head>
+
+<body>
+	<?php include('includes/header.php');?>
+
+	<div class="ts-main-content">
+		<?php include('includes/leftbar.php');?>
+		<div class="content-wrapper">
+			<div class="container-fluid">
+
+				<div class="row">
+					<div class="col-md-12">
+
+						<h2 class="page-title">Certificate Request</h2>
+
+						<!-- Zero Configuration Table -->
+						<div class="panel panel-default">
+							<div class="panel-heading">Bonafide Users</div>
+							<div class="panel-body">
+							<?php if($error){?><div class="errorWrap" id="msgshow"><?php echo htmlentities($error); ?> </div><?php } 
+				else if($msg){?><div class="succWrap" id="msgshow"><?php echo htmlentities($msg); ?> </div><?php }?>
+								<table id="zctb" class="display table table-striped table-bordered table-hover" cellspacing="0" width="100%">
+									<thead>
+										<tr>
+										<th>#</th>
+												<th>Full Name</th>
+												<th>Department</th>
+                                                <th>Semester</th>
+												<th>Enrollment No/Roll No</th>											
+                                                <th>Category</th>
+                                                <th>Start Date</th>                                            
+                                                <th>End Date</th>
+												<th>Purpose </th>
+												<th>Status </th>									
+												<th>Action</th>	
+												<th>download</th>
+										</tr>
+									</thead>
+									
+									<tbody>
+
+<?php $sql = "SELECT * from  bonafide where course='SSAIMT'";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{				?>	
+										<tr>
+											<td><?php echo htmlentities($cnt);?></td>											
+                                            <td><?php echo htmlentities($result->fname);?></td>
+											<td><?php echo htmlentities($result->department);?></td>
+											<td><?php echo htmlentities($result->semester);?></td>
+											<td><?php echo htmlentities($result->enroll);?></td>                                          
+                                            <td><?php echo htmlentities($result->category);?></td>   
+                                            <td><?php echo htmlentities($result->sterm);?></td>  
+											<td><?php echo htmlentities($result->eterm);?></td>
+                                            <td><?php echo htmlentities($result->purpose);?></td>  
+                                            <td>                                           
+                                            <?php if($result->status == 1)
+                                                    {?>
+                                                    <a href="bonafideuserlist.php?confirm=<?php echo htmlentities($result->id);?>" onclick="return confirm('Do you really want to Un-Confirm the Account')">Approved <i class="fa fa-check-circle"></i></a> 
+                                                    <?php } else {?>
+                                                    <a href="bonafideuserlist.php?unconfirm=<?php echo htmlentities($result->id);?>" onclick="return confirm('Do you really want to Confirm the Account')">Reject<i class="fa fa-times-circle"></i></a>
+                                                    <?php } ?>
+											</td>                                           											
+											<td>
+												<a href="edit-user.php?edit=<?php echo $result->id;?>" onclick="return confirm('Do you want to Edit');">&nbsp; <i class="fa fa-pencil"></i></a>&nbsp;&nbsp;
+												<a href="bonafideuserlist.php?del=<?php echo $result->id;?>&name=<?php echo htmlentities($result->email);?>" onclick="return confirm('Do you want to Delete');"><i class="fa fa-trash" style="color:red"></i></a>&nbsp;&nbsp;
+											</td>
+											<td>
+											<div>
+											<button type="button" class="btn btn-default btn-sm" name="create_pdf">
+											<span class="glyphicon glyphicon-download-alt"></span><a href="pdfcreater_ssaimt.php?id=<?php echo htmlentities($result->id);?>">&nbsp;&nbsp;Download Bonafide</a></button>
+											</div>   
+											</td>
+										</tr>
+										<?php $cnt=$cnt+1; }} ?>
+										
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- Loading Scripts -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap-select.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.dataTables.min.js"></script>
+	<script src="js/dataTables.bootstrap.min.js"></script>
+	<script src="js/Chart.min.js"></script>
+	<script src="js/fileinput.js"></script>
+	<script src="js/chartData.js"></script>
+	<script src="js/main.js"></script>
+	<script type="text/javascript">
+				 $(document).ready(function () {          
+					setTimeout(function() {
+						$('.succWrap').slideUp("slow");
+					}, 3000);
+					});
+		</script>
+		
+</body>
+</html>
+<?php } ?>
---
+++ admin/pdfcreater.php
@@ -0,0 +1,77 @@
+<?php  
+ 
+      $output = '';  
+      $connect = mysqli_connect("localhost", "root", "", "test");  
+	  $aeid=intval($_GET['id']);
+      $sql = "SELECT * FROM bonafide where id=$aeid";  
+      $result = mysqli_query($connect, $sql);  
+      while($row = mysqli_fetch_array($result))  
+      {          
+        $fname=$row["fname"];
+		$department=$row["department"];
+	    $sem=$row["semester"];
+	    $enroll=$row["enroll"];
+		$dob=$row["dob"];
+		$category=$row["category"];
+		$sterm=$row["sterm"];
+		$eterm=$row["eterm"];
+		$purpose=$row["purpose"];                   
+      }  
+      
+      require_once('tcpdf/tcpdf.php');  
+      $obj_pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+	  $pagelayout = array(100,100);
+	  $obj_pdf->setPageOrientation('L');
+      $obj_pdf->SetCreator(PDF_CREATOR); 
+      			
+      $obj_pdf->SetTitle("S.S. Agarwal Institute of Engineering and Technology, Navsari");  
+      $obj_pdf->SetHeaderData('', '', PDF_HEADER_TITLE, PDF_HEADER_STRING);  
+      $obj_pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));  
+      $obj_pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));  
+      $obj_pdf->SetDefaultMonospacedFont('helvetica');  
+      $obj_pdf->SetFooterMargin(PDF_MARGIN_FOOTER);  
+      $obj_pdf->SetMargins('47', '15', '30');  
+	  $obj_pdf->SetLineStyle( array( 'width' => 15, 'color' => array(0,0,0)));
+	  $obj_pdf->SetFillColor(255,255,127);
+	  $obj_pdf->SetTextColor(0,0,128);
+	  
+      $obj_pdf->setPrintHeader(false);  
+      $obj_pdf->setPrintFooter(false);  
+      $obj_pdf->SetAutoPageBreak(TRUE, 10);  
+      $obj_pdf->SetFont('helvetica', '', 13);  
+      $obj_pdf->AddPage();  
+      $content = '';  
+      $content .= '
+      
+      <table border="0" cellspacing="0" cellpadding="5" style="border-left: 1px solid #orange;border-top: 1px solid #orange;border-right: 1px solid #orange;border-bottom: 1px solid #orange" >  
+      
+	  <tr>
+      <td align="center"><img src="ssaiet_logo.jpg" width="100" height="100" ></td>
+	  <td colspan="3"><h3 align="center">Agrawal Education Foundation Managed</h3>
+      <h2 align="center">S.S. AGRAWAL INSTITUTE OF ENGINEERING & TECHNOLOGY,NAVSARI</h2>
+	  <h4 align="center">Campus : S.S. Agrawal College Campus, Veeranajali Marg,</h4>   
+	  <h4 align="center"> Near Devina Park Society, Gandevi Road, Navsari-396445, Gujarat. <br/>Phone No.: 02637-232667/232857 </h4>
+	  </td>
+	  </tr>
+	  
+	  <tr><td colspan="8"><h4 style="border-top-width: 1px #000; border-bottom-width: 1px #000;" align="center">Affiliated to Gujarat Technological University</h4></td></tr>
+	  
+	  <tr><td colspan="3" ><h4 align="left">Bonafide No.:</h4></td> <td colspan="4"><h4 align="left">Date: </h4></td></tr>  
+	  
+	  <tr><td colspan="8" style="line-height: 25.5em;">
+	  This is to certify that Mr./Miss. <b><u>' .$fname.' </u></b> is a bonafide student of our college studying in <b><u> '.$sem.' </u></b> semester of B.E. <b><u> '.$department.' </u></b>  having 
+      Enrollment No.: <b><u> '.$enroll.'</u></b>  and his/her date of birth is  <b><u> '.$dob.' </u></b>  belongs to SC / ST / OBC  <b><u> '.$category.' </u></b> category as per documents presented by him/her.</td></tr>
+      <tr><td colspan="8"> His/Her regular term is from  <b><u> '.$sterm.' </u></b> to <b><u>  '.$eterm.' </u></b> .</td></tr>
+	  <tr><td colspan="8"> This certificate is intended use for <b><u>  '.$purpose.'  </u></b> .  </td></tr> 
+	  <tr><td colspan="8"> As per our record he/she is having good charater.</td></tr>
+	  <tr><td colspan="8"></td></tr>
+      </table> 		 
+      ';
+      // output the HTML content
+   //    $obj_pdf->writeHTML($html, true, false, true, false, '');	 
+          
+       $obj_pdf->writeHTML($content);  
+       $obj_pdf->Output('sample.pdf', 'I');  
+   
+ ?>  
+ 
