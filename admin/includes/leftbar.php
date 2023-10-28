cdda1100cd8693144a9e03899786fed77b8ab9b7
Niyanta-Panchal <146424762+Niyanta-Panchal@users.noreply.github.com>
2023-10-28 03:26:28 -0700

    Add files via upload

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,30 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+			<li class="ts-label" style="font-family: Arial, Helvetica, sans-serif; font-size: 25px;"><center>Menu</center></li>
+			<li><a href="dashboard.php"style="font-family: Arial, Helvetica, sans-serif; font-size: 16px;"><i class="fa fa-dashboard"></i><b>Dashboard</a></b>
+			</li>
+			<li><a href="profile.php" style="font-family: Arial, Helvetica, sans-serif; font-size: 16px;"><i class="fa fa-user"></i><b>Profile</b><sup style="color:red">*</sup></a>
+			</li>
+			<li><a href="notification.php" style="font-family: Arial, Helvetica, sans-serif; font-size: 16px;"><i class="fa fa-bell"></i><b>Notification</b> <sup style="color:red">*</sup></a>
+			</li>
+			<li><a href="userlist.php" style="font-family: Arial, Helvetica, sans-serif; font-size: 16px;"><i class="fa fa-users"></i><b>Userlist</b><sup style="color:red">*</sup></a>
+			</li>
+	
+			<li class='has-submenu'style="font-family: Arial, Helvetica, sans-serif; font-size: 16px;"><a class='prett' href='Dropdown 1' title='Dropdown 1'><i class="fa fa-users"></i><b>Bonafide Request</b><sup style="color:red">*</sup></a>
+            <ul class='submenu'>
+            <li><a href="bonafide_ssacm.php" title="Sub Menu">SSACM</a></li>
+            <li><a href="bonafide_ssaimt.php" title="Sub Menu">SSAIMT</a></li>
+            <li><a href="bonafideuserlist.php" title="Sub Menu">SSAIET</a></li>
+            </ul>
+            </li>
+							
+			<li><a href="loruserlist.php"style="font-family: Arial, Helvetica, sans-serif; font-size: 16px;"><i class="fa fa-users"></i><b>LOR Request</b><sup style="color:red">*</sup></a>
+			</li> 
+			<li><a href="deleteduser.php"style="font-family: Arial, Helvetica, sans-serif; font-size: 16px;"><i class="fa fa-user-times"></i><b>Deleted Users</b><sup style="color:red">*</sup></a>
+			</li>			
+			</ul>
+			<p class="text-center" style="color:#12877c; margin-top: 100px; font-family: Arial, Helvetica, sans-serif; font-size: 20px">© SSAIET-NAVSARI</p>
+		</nav>
+
+		
